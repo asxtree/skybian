57e548cf01afc67df0945f59e5edf77fe73b215b
asxtree <cozia.stefan@gmail.com>
2020-04-29 18:11:26 +0300

    Update build.sh for Raspbian
    initial commit

--- build.sh
+++ build.sh
@@ -26,15 +26,15 @@ FINAL_IMG_DIR=${ROOT}/output/final
 BASE_IMG=${IMAGE_DIR}/base_image
 
 # Download directories.
-PARTS_ARMBIAN_DIR=${PARTS_DIR}/armbian
+PARTS_RASPBIAN_DIR=${PARTS_DIR}/raspbian
 PARTS_SKYWIRE_DIR=${PARTS_DIR}/skywire
 PARTS_TOOLS_DIR=${PARTS_DIR}/tools
 
 # Image related variables.
-ARMBIAN_IMG_7z=""
-ARMBIAN_IMG=""
-ARMBIAN_VERSION=""
-ARMBIAN_KERNEL_VERSION=""
+RASPBIAN_IMG_7z=""
+RASPBIAN_IMG=""
+RASPBIAN_VERSION=""
+RASPBIAN_KERNEL_VERSION=""
 
 # Loop device.
 IMG_LOOP="" # free loop device to be used.
@@ -129,7 +129,7 @@ create_folders()
     info "Creating output folder structure..."
     mkdir -p "$FINAL_IMG_DIR"
     mkdir -p "$FS_MNT_POINT"
-    mkdir -p "$PARTS_DIR" "$PARTS_ARMBIAN_DIR" "$PARTS_SKYWIRE_DIR" "$PARTS_TOOLS_DIR"
+    mkdir -p "$PARTS_DIR" "$PARTS_RASPBIAN_DIR" "$PARTS_SKYWIRE_DIR" "$PARTS_TOOLS_DIR"
     mkdir -p "$IMAGE_DIR"
 
     info "Done!"
@@ -143,7 +143,7 @@ get_tools()
   info "Building skyconf..."
   info "_src=$_src"
   info "_out=$_out"
-  env GOOS=linux GOARCH=arm64 GOARM=7 go build -o "$_out" -v "$_src" || return 1
+  env GOOS=linux GOARCH=arm GOARM=7 go build -o "$_out" -v "$_src" || return 1
 
   info "Done!"
 }
@@ -172,29 +172,29 @@ get_skywire()
   info "Done!"
 }
 
-download_armbian()
+download_raspbian()
 {
-  local _DST=${PARTS_ARMBIAN_DIR}/armbian.7z # Download destination file name.
+  local _DST=${PARTS_RASPBIAN_DIR}/raspbian.7z # Download destination file name.
 
-  info "Downloading image from ${ARMBIAN_DOWNLOAD_URL} to ${_DST} ..."
-  wget -c "${ARMBIAN_DOWNLOAD_URL}" -O "${_DST}" ||
+  info "Downloading image from ${RASPBIAN_DOWNLOAD_URL} to ${_DST} ..."
+  wget -c "${RASPBIAN_DOWNLOAD_URL}" -O "${_DST}" ||
     (error "Download failed." && return 1)
 }
 
 # Get the latest ARMBIAN image for Orange Pi Prime
-get_armbian()
+get_raspbian()
 {
-  local ARMBIAN_IMG_7z="armbian.7z"
+  local RASPBIAN_IMG_7z="raspbian.7z"
 
     # change to dest dir
-    cd "${PARTS_ARMBIAN_DIR}" ||
+    cd "${PARTS_RASPBIAN_DIR}" ||
       (error "Failed to cd." && return 1)
 
     # user info
-    info "Getting Armbian image, clearing dest dir first."
+    info "Getting Raspbian image, clearing dest dir first."
 
     # test if we have a file in there
-    if [ -r armbian.7z ] ; then
+    if [ -r raspbian.7z ] ; then
 
         # use already downloaded image file
         notice "Reusing already downloaded file"
@@ -203,53 +203,53 @@ get_armbian()
         info "No cached image, downloading.."
 
         # download it
-        download_armbian
+        download_raspbian
     fi
 
     # extract and check it's integrity
-    info "Armbian file to process is '${ARMBIAN_IMG_7z}'."
+    info "Raspbian file to process is '${RASPBIAN_IMG_7z}'."
 
     # check if extracted image is in there to save time
-    if [ -n "$(ls Armbian*.img || true)" ] ; then
+    if [ -n "$(ls *raspbian*.img || true)" ] ; then
         # image already extracted nothing to do
-        notice "Armbian image already extracted"
+        notice "Raspbian image already extracted"
     else
         # extract armbian
         info "Extracting image..."
-        if ! 7z e "${ARMBIAN_IMG_7z}" ; then
+        if ! 7z e "${RASPBIAN_IMG_7z}" ; then
             error "Extracting failed, file is corrupt? Re-run the script to get it right."
-            rm "${ARMBIAN_IMG_7z}" &> /dev/null || true
+            rm "${RASPBIAN_IMG_7z}" &> /dev/null || true
             exit 1
         fi
     fi
 
     # check integrity
-    info "Testing image integrity..."
-    if ! $(command -v sha256sum) -c --status -- *.sha ; then
-        error "Integrity of the image is compromised, re-run the script to get it right."
-        rm -- *img *txt *sha *7z &> /dev/null || true
-        exit 1
-    fi
+    #info "Testing image integrity..."
+    #if ! $(command -v sha256sum) -c --status -- *.sha ; then
+    #    error "Integrity of the image is compromised, re-run the script to get it right."
+    #    rm -- *img *txt *sha *7z &> /dev/null || true
+    #    exit 1
+    #fi
 
     # get image filename
-    ARMBIAN_IMG=$(ls Armbian*.img || true)
+    RASPBIAN_IMG=$(ls *raspbian*.img || true)
 
     # imge integrity
-    info "Image integrity assured via sha256sum."
-    notice "Final image file is ${ARMBIAN_IMG}"
+    #info "Image integrity assured via sha256sum."
+    notice "Final image file is ${RASPBIAN_IMG}"
 
     # get version & kernel version info
-    ARMBIAN_VERSION=$(echo "${ARMBIAN_IMG}" | awk -F '_' '{ print $2 }')
-    ARMBIAN_KERNEL_VERSION=$(echo "${ARMBIAN_IMG}" | awk -F '_' '{ print $6 }' | rev | cut -d '.' -f2- | rev)
+    #ARMBIAN_VERSION=$(echo "${ARMBIAN_IMG}" | awk -F '_' '{ print $2 }')
+    #ARMBIAN_KERNEL_VERSION=$(echo "${ARMBIAN_IMG}" | awk -F '_' '{ print $6 }' | rev | cut -d '.' -f2- | rev)
 
     # info to the user
-    notice "Armbian version: ${ARMBIAN_VERSION}"
-    notice "Armbian kernel version: ${ARMBIAN_KERNEL_VERSION}"
+    #notice "Armbian version: ${ARMBIAN_VERSION}"
+    #notice "Armbian kernel version: ${ARMBIAN_KERNEL_VERSION}"
 }
 
 get_all()
 {
-  get_armbian || return 1
+  get_raspbian || return 1
   get_skywire || return 1
   get_tools || return 1
 }
@@ -304,7 +304,7 @@ prepare_base_image()
 
   # copy armbian image to base image location
   info "Copying base image..."
-  cp "${PARTS_DIR}/armbian/${ARMBIAN_IMG}" "${BASE_IMG}" || return 1
+  cp "${PARTS_DIR}/raspbian/${RASPBIAN_IMG}" "${BASE_IMG}" || return 1
 
   # Add space to base image
   if [[ "$BASE_IMG_ADDED_SPACE" -ne "0" ]]; then
@@ -340,13 +340,13 @@ copy_to_img()
   sudo cp -rf "$PARTS_TOOLS_DIR"/* "$FS_MNT_POINT"/usr/bin/ || return 1
 
   # Copy scripts
-  info "Copying disable user creation script..."
-  sudo cp -f "${ROOT}/static/armbian-check-first-login.sh" "${FS_MNT_POINT}/etc/profile.d/armbian-check-first-login.sh" || return 1
-  sudo chmod +x "${FS_MNT_POINT}/etc/profile.d/armbian-check-first-login.sh" || return 1
-  info "Copying headers (so OS presents itself as Skybian)..."
-  sudo cp "${ROOT}/static/10-skybian-header" "${FS_MNT_POINT}/etc/update-motd.d/" || return 1
-  sudo chmod +x "${FS_MNT_POINT}/etc/update-motd.d/10-skybian-header" || return 1
-  sudo cp -f "${ROOT}/static/armbian-motd" "${FS_MNT_POINT}/etc/default" || return 1
+  #info "Copying disable user creation script..."
+  #sudo cp -f "${ROOT}/static/armbian-check-first-login.sh" "${FS_MNT_POINT}/etc/profile.d/armbian-check-first-login.sh" || return 1
+  #sudo chmod +x "${FS_MNT_POINT}/etc/profile.d/armbian-check-first-login.sh" || return 1
+  #info "Copying headers (so OS presents itself as Skybian)..."
+  #sudo cp "${ROOT}/static/10-skybian-header" "${FS_MNT_POINT}/etc/update-motd.d/" || return 1
+  #sudo chmod +x "${FS_MNT_POINT}/etc/update-motd.d/10-skybian-header" || return 1
+  #sudo cp -f "${ROOT}/static/armbian-motd" "${FS_MNT_POINT}/etc/default" || return 1
 
   # Copy systemd units
   info "Copying systemd unit services..."
@@ -440,7 +440,7 @@ clean_image()
 clean_output_dir()
 {
   # Clean parts.
-  cd "${PARTS_ARMBIAN_DIR}" && find . -type f ! -name '*.7z' -delete
+  cd "${PARTS_RASPBIAN_DIR}" && find . -type f ! -name '*.7z' -delete
   cd "${PARTS_SKYWIRE_DIR}" && find . -type f ! -name '*.tar.gz' -delete && rm -rf bin
   cd "${FINAL_IMG_DIR}" && find . -type f ! -name '*.tar.xz' -delete
 
@@ -458,7 +458,7 @@ build_disk()
   cd "${IMAGE_DIR}" || return 1
 
   # final name
-  local NAME="Skybian-${VERSION}"
+  local NAME="SkyRaspbian-${VERSION}"
 
   # info
   info "Building image for ${NAME}"
