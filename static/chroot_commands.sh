81f6d797d102b2f2a4f4fa8e2541c3cd151f2185
asxtree <cozia.stefan@gmail.com>
2020-05-03 09:58:05 +0300

    Update chroot_commands.sh
    Added the network manager installation

--- static/chroot_commands.sh
+++ static/chroot_commands.sh
@@ -13,8 +13,8 @@ function info() {
 
 
 # change root password to the default of: skybian
-info "Setting default root password to 'skybian'..."
-printf "skybian\nskybian\n" | passwd root
+info "Setting default pi password to 'skybian'..."
+printf "skybian\nskybian\n" | passwd pi
 
 # by default update the es_US locales
 info "Re-generating the locales info for en_US.UTF-8..."
@@ -24,16 +24,16 @@ locale-gen en_US.UTF-8
 # modify and un-comment
 info "Updating your system via APT"
 export DEBIAN_FRONTEND=noninteractive
-apt-get -y update
-#apt-get -y install [your_pkgs_here]
+sudo apt-get -y update
+sudo apt-get -y install network-manager toilet figlet
 #apt-get -y remove --purge [your_pkgs_here]
 # keep this on the very end of this block
 info "Cleaning the APT cache to make a smaller image"
-apt-get clean
+sudo apt-get clean
 
 # forge a time on the system to avoid fs dates are in the future
-info "Setting the chroot clock to now to avoid bugs with the date..."
-/sbin/fake-hwclock save force
+#info "Setting the chroot clock to now to avoid bugs with the date..."
+#/sbin/fake-hwclock save force
 
 # Mkdir
 mkdir -p /etc/skywire-hypervisor || 0
